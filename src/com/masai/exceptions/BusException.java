424d6245bfd0220a03878a3d028208417af38973
Mhendrapratap <mahendrapratp354@gmail.com>
2023-02-24 21:35:10 +0530

    day-3

---
+++ src/com/masai/exceptions/BusException.java
@@ -0,0 +1,10 @@
+package com.masai.exceptions;
+
+public class BusException extends Exception{
+	public BusException() {
+		
+	}
+	public BusException(String ex) {
+		super (ex);
+	}
+}
---
+++ src/com/masai/exceptions/CustomerExceptions.java
@@ -0,0 +1,11 @@
+package com.masai.exceptions;
+
+public class CustomerExceptions extends Exception{
+public CustomerExceptions(){
+		
+	}
+	
+	public CustomerExceptions(String str){
+		super(str);
+	}
+}
